f2ca04825f389fe07ada239ce8af6ad8a1a6d5bb
jry <ijry@qq.com>
2019-01-23 13:49:30 +0800

    封装动态页面列表组件

---
+++ src/views/components/va_dypage/va_dylist.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <Card shadow>
+      <Button
+        v-for="(item,key) in dynamic_data.top_button_list"
+        :key="key"
+        :type="item.type"
+        :size="item.size"
+        :icon="item.icon"
+        :shape="item.shape"
+        @click="top_button_modal(key)"
+        style="margin-bottom: 15px;">
+        {{item.title}}
+      </Button>
+      <tree-table
+        expand-key="title"
+        :is-fold="false"
+        :border="true"
+        :stripe="true"
+        :expand-type="false"
+        :selectable="false"
+        :columns="dynamic_data.columns"
+        :data="data_list" >
+        <template slot="right_button_list" slot-scope="scope">
+            <Button
+                v-for="(item,key) in dynamic_data.right_button_list"
+                :key="key"
+                :type="item.type"
+                :size="item.size"
+                :icon="item.icon"
+                :shape="item.shape"
+                @click="right_button_modal(key,scope)"
+                style="margin-right: 3px;">
+                {{item.title}}
+            </Button>
+        </template>
+      </tree-table>
+    </Card>
+  </div>
+</template>
+
+<script>
+import VaDyform from '@/views/components/va_dypage/va_dyform'
+export default {
+    name: 'DynamicList',
+    props: {
+        api: ''
+    },
+    components: {
+        VaDyform
+    },
+    data () {
+        return {
+            data_list: [],
+            dynamic_data: {}
+        }
+    },
+    created() {
+    },
+    mounted(){
+        let _this = this
+        axios.get(this.api)
+            .then(function (res) {
+                res = res.data
+                if(res.code=='200'){
+                    _this.dynamic_data = res.data.dynamic_data
+                    _this.data_list = res.data.data_list
+                }else{
+                    _this.$Message.error(res.msg)
+                }
+            })
+            .catch(function (error) {
+                console.log(error)
+            });
+    },
+    methods: {
+        top_button_modal(key) {
+            let _this = this
+            let button_data = _this.dynamic_data.top_button_list[key]
+            this.$Modal.success({
+                width: button_data.width?button_data.width:600,
+                scrollable: true,
+                title: button_data.title?button_data.title:'未指定标题',
+                iconName: 'ios-create-outline',
+                render: (h) => {
+                    return h(VaDyform, {
+                        props: {
+                            api: button_data.api
+                        },
+                        on: {
+                        }
+                    })
+                }
+            })
+        },
+        right_button_modal(key,scope) {
+            let _this = this
+            let button_data = _this.dynamic_data.right_button_list[key]
+            if(button_data.modalType == 'confirm'){
+                this.$Modal.confirm({
+                    okText: button_data.okText,
+                    cancelText: button_data.cancelText,
+                    title: button_data.modalTitle,
+                    content: button_data.content,
+                    onOk: () => {
+                        axios.delete(button_data.api + '/' + scope.row.id)
+                            .then(function (res) {
+                                res = res.data
+                                if(res.code=='200'){
+                                    _this.$Message.success(res.msg)
+                                }else{
+                                    _this.$Message.error(res.msg)
+                                }
+                            })
+                            .catch(function (error) {
+                                console.log(error)
+                            });
+                    },
+                    onCancel: () => {
+                    }
+                });
+            } else {
+                this.$Modal.warning({
+                    width: button_data.width?button_data.width:600,
+                    scrollable: true,
+                    title: button_data.modalTitle?button_data.modalTitle:'未指定标题',
+                    render: (h) => {
+                        return h(VaDyform, {
+                            props: {
+                                api: button_data.api + '/' + scope.row.id
+                            },
+                            on: {
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    }
+}
+</script>
+    
+<style>
+    .ivu-modal-confirm .ivu-modal-confirm-footer{
+        /* display: none; */
+    }
+</style>
--- src/views/module/core/auth_role/lists.vue
+++ src/views/module/core/auth_role/lists.vue
@@ -1,140 +1,26 @@
 <template>
   <div>
-    <Card shadow>
-      <Button @click="modal_add()">添加</Button>
-      <tree-table expand-key="title" :is-fold="false" :border="true" :stripe="true" :expand-type="false" :selectable="false" :columns="columns" :data="data_list" >
-        <template slot="actions" slot-scope="scope">
-          <Button @click="modal_edit(scope)">修改</Button>
-          <Button @click="modal_delete(scope)">删除</Button>
-        </template>
-      </tree-table>
-    </Card>
+    <VaDylist api="v1/core/admin/auth_role/lists"></VaDylist>
   </div>
 </template>
 
 <script>
-import VaDyform from '@/views/components/va_dypage/va_dyform'
+import VaDylist from '@/views/components/va_dypage/va_dylist'
 export default {
     name: 'tree_table_page',
     components: {
-        VaDyform
+        VaDylist
     },
     data () {
         return {
-            columns: [
-                {
-                title: 'ID',
-                key: 'id',
-                width: '40px'
-                },
-                {
-                title: '部门',
-                key: 'title',
-                minWidth: '50px'
-                },
-                {
-                title: '排序',
-                key: 'sortnum'
-                },
-                {
-                title: '操作',
-                key: 'actions',
-                minWidth: '200px',
-                type: 'template',
-                template: 'actions'
-                }
-            ],
-            data_scope: null,
-            data_list: [],
-            form_api:{}
         }
     },
     created(){
-        let _this = this
-        axios.get('v1/core/admin/auth_role/lists')
-            .then(function (res) {
-                res = res.data
-                if(res.code=='200'){
-                    _this.form_api = res.data.form_api
-                    _this.data_list = res.data.data_list
-                    console.log(_this.form_api)
-                }else{
-                    _this.$Message.error(res.msg)
-                }
-            })
-            .catch(function (error) {
-                console.log(error)
-            });
     },
     methods: {
-        modal_add() {
-            let _this = this
-            this.$Modal.success({
-                width: '600',
-                scrollable: true,
-                title: '添加角色',
-                iconName: 'ios-create-outline',
-                render: (h) => {
-                    return h(VaDyform, {
-                        props: {
-                            api: _this.form_api.add
-                        },
-                        on: {
-                        }
-                    })
-                }
-            })
-        },
-        modal_edit(scope) {
-            let _this = this
-            this.$Modal.warning({
-                width: '600',
-                scrollable: true,
-                title: '修改角色',
-                iconName: 'ios-create-outline',
-                render: (h) => {
-                    return h(VaDyform, {
-                        props: {
-                            api: _this.form_api.edit + '/' + scope.row.id
-                        },
-                        on: {
-                        }
-                    })
-                }
-            })
-        },
-        modal_delete(scope) {
-            let _this = this
-            this.$Modal.confirm({
-                okText: "确认删除",
-                cancelText: "取消操作",
-                title: '确认要删除该角色吗？',
-                content: '<p><p>如果该角色下有子角色需要先删除或者移动</p><p>如果该角色下有成员需要先移除才可以删除</p><p>删除该角色将会删除对应的权限数据</p></p>',
-                onOk: () => {
-                    _this.$Message.info('正在删除');
-                    axios.delete(_this.form_api.delete + '/' + scope.row.id)
-                        .then(function (res) {
-                            res = res.data
-                            if(res.code=='200'){
-                                _this.$Message.success(res.msg)
-                            }else{
-                                _this.$Message.error(res.msg)
-                            }
-                        })
-                        .catch(function (error) {
-                            console.log(error)
-                        });
-                },
-                onCancel: () => {
-                }
-            });
-        }
     }
 }
 </script>
     
 <style>
-    .ivu-modal-confirm .ivu-modal-confirm-footer{
-        /* display: none; */
-    }
 </style>
